490452f8c389931d0ca4bcc788a9641f0fcd34dd
kkssgg114514 <2524183995@qq.com>
2026-03-26 14:26:49 +0800

    Refine China Mobile splash skip rule

--- src/apps/com.greenpoint.android.mc10086.activity.ts
+++ src/apps/com.greenpoint.android.mc10086.activity.ts
@@ -7,7 +7,8 @@ export default defineGkdApp({
     {
       key: 0,
       name: '开屏广告',
-      desc: '点击右上角“跳过”按钮',
+      desc: '点击右上角跳过按钮',
+      activityIds: '.view.tabs.AppTabFragment',
       fastQuery: true,
       forcedTime: 10_000,
       matchRoot: true,
@@ -19,36 +20,20 @@ export default defineGkdApp({
         {
           key: 0,
           anyMatches: [
-            '@[clickable=true] > [text="跳过"][visibleToUser=true]',
-            '@[clickable=true] > [desc="跳过"][visibleToUser=true]',
-            '@[clickable=true] > [text*="跳过"][text.length<8][visibleToUser=true]',
-            '@[clickable=true] > [desc*="跳过"][desc.length<8][visibleToUser=true]',
+            '[id="com.greenpoint.android.mc10086.activity:id/top_view_right_operation"][clickable=true][visibleToUser=true]',
+            '[vid="top_view_right_operation"][clickable=true][visibleToUser=true]',
           ],
         },
         {
           key: 1,
-          action: 'clickCenter',
           anyMatches: [
-            '[text*="跳过"][text.length<8][width<300 && height<200][visibleToUser=true]',
-            '[desc*="跳过"][desc.length<8][width<300 && height<200][visibleToUser=true]',
+            '@[clickable=true] > [text="跳过"][visibleToUser=true]',
+            '@[clickable=true] > [desc="跳过"][visibleToUser=true]',
+            '@[clickable=true] > [text*="跳过"][text.length<8][visibleToUser=true]',
+            '@[clickable=true] > [desc*="跳过"][desc.length<8][visibleToUser=true]',
+            '[text*="跳过"][text.length<8][width<420 && height<220][visibleToUser=true]',
+            '[desc*="跳过"][desc.length<8][width<420 && height<220][visibleToUser=true]',
           ],
-        },
-        {
-          key: 2,
-          matches: '([text*="广告"][visibleToUser=true]) && (@[parent=null])',
-          position: {
-            right: 'width*0.07',
-            top: 'height*0.09',
-          },
-        },
-        {
-          key: 3,
-          matches:
-            '(([text*="立即前往"][visibleToUser=true]) || ([desc*="立即前往"][visibleToUser=true])) && (@[parent=null])',
-          position: {
-            right: 'width*0.07',
-            top: 'height*0.09',
-          },
         },
       ],
     },
