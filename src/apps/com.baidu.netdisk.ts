1ef4caa5e58f66cbc2738b845be079f4fa684f54
kkssgg114514 <2524183995@qq.com>
2026-03-26 11:25:35 +0800

    Improve Baidu Netdisk splash ad skip rule

--- src/apps/com.baidu.netdisk.ts
+++ src/apps/com.baidu.netdisk.ts
@@ -7,23 +7,35 @@ export default defineGkdApp({
     {
       key: 0,
       name: '开屏广告',
-      desc: '点击右上角“跳过广告/跳过/关闭”按钮',
+      desc: '点击左上或右上圆圈“跳过”按钮',
       fastQuery: true,
+      forcedTime: 10_000,
       matchRoot: true,
       matchTime: 10_000,
       actionMaximum: 1,
+      priorityTime: 10_000,
       resetMatch: 'app',
-      rules: {
-        anyMatches: [
-          '[text*="跳过广告"]',
-          '[text*="跳过"]',
-          '[desc*="跳过广告"]',
-          '[desc*="跳过"]',
-          '[text="关闭"]',
-          '[desc="关闭"]',
-        ],
-        action: 'clickCenter',
-      },
+      rules: [
+        {
+          key: 0,
+          anyMatches: [
+            '@[clickable=true] > [text="跳过"][visibleToUser=true]',
+            '@[clickable=true] > [desc="跳过"][visibleToUser=true]',
+            '@[clickable=true] > [text*="跳过"][text.length<8][visibleToUser=true]',
+            '@[clickable=true] > [desc*="跳过"][desc.length<8][visibleToUser=true]',
+          ],
+        },
+        {
+          key: 1,
+          action: 'clickCenter',
+          anyMatches: [
+            '[text="跳过"][width<300 && height<200][visibleToUser=true]',
+            '[desc="跳过"][width<300 && height<200][visibleToUser=true]',
+            '[text*="跳过"][text.length<8][width<300 && height<200][visibleToUser=true]',
+            '[desc*="跳过"][desc.length<8][width<300 && height<200][visibleToUser=true]',
+          ],
+        },
+      ],
     },
   ],
 });
