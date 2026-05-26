583ff91e5f8c316a6c61381f134b0bb73702c34a
kkssgg114514 <2524183995@qq.com>
2026-05-26 16:57:38 +0800

    Fix Amap splash activity targeting

--- src/apps/com.autonavi.minimap.ts
+++ src/apps/com.autonavi.minimap.ts
@@ -8,7 +8,7 @@ export default defineGkdApp({
       key: 0,
       name: '开屏广告',
       desc: '点击右上角“跳过广告”按钮',
-      activityIds: '.map.activity.NewMapActivity',
+      activityIds: 'com.autonavi.map.activity.NewMapActivity',
       fastQuery: true,
       forcedTime: 10_000,
       matchRoot: true,
@@ -28,6 +28,15 @@ export default defineGkdApp({
           key: 1,
           matches:
             '([vid="fl_splash_container"][visibleToUser=true]) && ([text*="互动跳转详情"][visibleToUser=true]) && (@[clickable=true] > [text*="跳过"][visibleToUser=true])',
+        },
+        {
+          key: 2,
+          matches:
+            '([vid="fl_splash_container"][visibleToUser=true]) && ([text="跳过广告"][visibleToUser=true]) && (@[parent=null])',
+          position: {
+            right: 'width*0.12',
+            top: 'height*0.105',
+          },
         },
       ],
     },
